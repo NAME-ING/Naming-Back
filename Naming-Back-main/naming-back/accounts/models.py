be512bba45f385125a475c841f6070a333089711
CHIPCHIP <sinisu3083@naver.com>
2023-01-13 03:19:41 +0900

    [fix] 회원가입loginId 오류 수정/ firstname 오류 발생

--- Naming-Back-main/naming-back/accounts/models.py
+++ Naming-Back-main/naming-back/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    firstName = models.CharField(
-        max_length=20, unique=False, null=False)
     loginId = models.CharField(
         max_length=30, unique=True, null=False, default='')
+    firstName = models.CharField(
+        max_length=20, null=False, unique=False, default='')
--- Naming-Back-main/naming-back/accounts/serializers.py
+++ Naming-Back-main/naming-back/accounts/serializers.py
@@ -6,11 +6,13 @@ from rest_framework import serializers
 class RegisterForm(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'loginId', 'password']
+        fields = ['id', 'loginId', 'firstName', 'password']
 
     def create(self, validated_data):
         user = User.objects.create(
-            userid=validated_data['loginId'],)
+            loginId=validated_data['loginId'],)
+        name = User.objects.create(
+            firstName=validated_data['firstName'], )
         user.set_password(validated_data['password'])
         user.save()
 
@@ -18,7 +20,7 @@ class RegisterForm(serializers.ModelSerializer):
 
 
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=64)
+    loginId = serializers.CharField(max_length=30)
     password = serializers.CharField(max_length=128, write_only=True)
 
     def validate(self, data):
